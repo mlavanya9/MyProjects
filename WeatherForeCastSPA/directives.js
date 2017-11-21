7710616405533c15d5328de07a441163d2fc0a79
mlavanya9 <mlavanya915@gmail.com>
2017-11-20 19:07:23 -0600

    new

---
+++ WeatherForeCastSPA/directives.js
@@ -0,0 +1,15 @@
+﻿//directive
+weatherApp.directive("weatherReport", function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        //isolate scope
+        scope: {
+            weatherDay: "=",
+            convertToStandard: "&",
+            convertToDate: "&",
+            dateFormat: "@"
+        }
+    }
+});
